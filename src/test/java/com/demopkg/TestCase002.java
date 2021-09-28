1b5bb9fe56f8cdd698eb1873025a2e78e8e78886
Agnii <sekshinge97@gmail.com>
2021-09-28 16:19:55 +0530

    TestCase002 New code Added

--- src/test/java/com/demopkg/TestCase002.java
+++ src/test/java/com/demopkg/TestCase002.java
@@ -1,10 +1,16 @@
 package com.demopkg;
 
-public class TestCase002 {
-	
-	public void m1() {
-		// TODO Auto-generated method stub
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCase002 {
+	@Test
+	public void m1() {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://testingshastra.com/");
 	}
 
 }
